a76bf71758f2173ab41f2dd69a72ada6e8fb935d
Stanislav <drovosek0317@gmail.com>
2022-09-04 15:17:57 +0300

    game_v3.py algorithm optimization (5 attempts instead of 6); code checking by pylint linter: score 10/10

--- project_1/game_v3.py
+++ project_1/game_v3.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def guess_number(number:int=1) -> int:
+def guess_number(number: int = 1) -> int:
     """Угадывает число не более чем за 20 попыток.
 
     Args:
@@ -12,42 +12,32 @@ def guess_number(number:int=1) -> int:
     Returns:
         int: Количество попыток
     """
-    
+
     # количество попыток
     count = 0
-    # границы "диапазона угадывания"
-    low_board = 1
-    high_board = 101
-    
+    # нижняя и верхняя границы "диапазона угадывания"
+    # (lower bound and upper bound)
+    l_bound, u_bound = 1, 101
+
     while True:
         count += 1
-        if (answer := low_board + (high_board - low_board)//2) == number:
+        if (answer := l_bound + (u_bound - l_bound)//2) == number:
             return count
-        low_board, high_board = ((answer, high_board) if answer < number else 
-                                 (low_board, answer))
-         
-        count += 1
-        if (answer := np.random.randint(low_board, high_board)) == number:
-            return count
-        low_board, high_board = ((answer, high_board) if answer < number else 
-                                 (low_board, answer))
-        
-    
-        
-        
+        l_bound, u_bound = ((answer, u_bound) if answer < number else
+                            (l_bound, answer))
 
 
 def score_game(random_predict) -> int:
-    """Среднее количество попыток, за которое функция угадывает число от 1 до 100.
-    Определяется за 1000 вызовов функции.
+    """Среднее количество попыток, за которое функция
+    угадывает число от 1 до 100. Определяется за 1000 вызовов функции.
 
     Args:
         random_predict (function): функция, которая угадывает число.
 
     Returns:
-        int: среднее количество попыток 
+        int: среднее количество попыток
     """
-    
+
     np.random.seed()
     random_array = np.random.randint(1, 101, size=1000)
     score = int(np.mean([random_predict(number) for number in random_array]))
@@ -55,6 +45,6 @@ def score_game(random_predict) -> int:
     return score
 
 
-# RUN 
+# RUN
 if __name__ == '__main__':
     score_game(guess_number)
